5702699fb9c7e7cded9388331f2f5127269172ca
mafutka <mafutka@gmail.com>
2026-02-16 22:41:40 +0200

    footer-modified

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
   <div className={css.content}>
     <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
     <div className={css.wrap}>
-      <p>Developer: your name</p>
+      <p>Developer: Maria Skobeleva</p>
       <p>
         Contact us:
-        <a href="<mailto:student@notehub.app>">student@notehub.app</a>
+        <a href="<mailto:student@notehub.app>">mafutka@gmail.com</a>
       </p>
     </div>
   </div>
